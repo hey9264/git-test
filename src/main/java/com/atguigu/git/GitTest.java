a682357c379787b7d94a888dc1ebf0458068df6f
Hey <hey9264@163.com>
2022-10-26 20:18:55 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author 就图个乐呗
+ * @version 1.0
+ * @date 2022/10/26
+ * @description 这是第一次测试IDEA集成Git
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
